69fec65b3472247c745afba0432607c1a02d7e7f
Mason Burke <masonburke@rocketmail.com>
2020-05-04 15:00:57 -0600

    welcome html added

--- Develop/server.js
+++ Develop/server.js
@@ -21,10 +21,6 @@ var routes = require("./controllers/questions_controller.js");
 
 // app.use(routes);
 
-
-
-
-
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
   // Log (server-side) when our server has started
