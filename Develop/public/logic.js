31c064ca073ff2a5aedd1467bd1f541c81116d4c
Tao <tao8261@aol.com>
2020-05-04 14:14:19 -0600

    started building logic in logic.js

--- Develop/public/logic.js
+++ Develop/public/logic.js
@@ -1,1 +1,11 @@
 //adding this file to make sure that github is linked
+
+$(function() {
+      $(".startQuiz").on("click", function() {
+            var newUser = {
+                  userName: $(".nameInput").val().trim(),
+                  quizCategory: $(".categorySelected")
+            }
+
+      })
+});
