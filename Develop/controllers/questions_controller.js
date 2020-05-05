275e30784bd678fd45db1c12388d7597f8c86c0b
Tymothy Sellers <tsellers1990@gmail.com>
2020-05-05 09:58:28 -0600

    fixing merge conflicts

---
+++ Develop/controllers/questions_controller.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/", function(req, res) {
+    //insert some stuff in here for the landing page
+})
+
+router.post("/api/questions", function (req, res) {
+    //insert some stuff in here for post's if we end up using them :)
+    //reference things in here and post to the page, ie like updates to the questions table ect
+})
+
+router.put("/api/questions/:id", function(req, res) {
+    //insert some stuff in here for the put's. ideally like targeting the q id and putting it on the page
+    //this would be for updates of the score in the DB ect <3
+})
+
+router.delete("/api/questions/:id", function(req, res) {
+    //ya know... put the delete stuff here if we need it
+    //not quick maths tbh
+})
+
+module.exports = router;
