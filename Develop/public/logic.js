e6627bf24f1de5c69518736dc3025c56fc46d47f
Tao <tao8261@aol.com>
2020-05-05 12:23:59 -0600

    commit before merging latest master

--- Develop/public/logic.js
+++ Develop/public/logic.js
@@ -10,35 +10,51 @@ var newSession = {
 }
 var currentScore = $("<div>").val(newSession.score)
 
-var scienceQuestions = [
-      {
-        question: 'How many feet are in 100 yards?',
-        answers: [
-          { text: '300', correct: true },
-          { text: '318.65', correct: false },
-          { text: '303.33', correct: false },
-          { text: '366.66', correct: false }
-        ]
-      },
-      {
-        question: 'How many miles are in 1 Nautical League?',
-        answers: [
-          { text: '6.215 miles', correct: false },
-          { text: '3.45 miles', correct: true },
-          { text: '12.12 miles', correct: false },
-          { text: '9.02 miles', correct: false },
-        ]
-      },
-      {
-        question: 'What is the largest planet in the solar system?',
-        answers: [
-          { text: 'Jupiter', correct: true },
-          { text: 'Saturn', correct: false },
-          { text: 'Uranus', correct: false },
-          { text: 'Pluto', correct: false }
-        ]
-      }
-    ]
+var scienceQuestions = {
+    
+      questions: {
+            q1: 'How many feet are in 100 yards?',
+            q2: 'Which do you prefer as a pet?',
+            q3: 'Baked Potato or Mashed?',
+            q4: 'Flip Flops or Hiking Boots?',
+          },
+      options: {
+            q1: ['300', '318.5', '327.21', '333.33'],
+            q2: ['Cute Dog', 'Cat'],
+            q3: ['mashed potato', 'Baked Spud'],
+            q4: ['Beach Flip Flops', 'Hiking Boots'],
+          }
+
+}
+// var scienceQuestions = [
+//       {
+//         questions: 'How many feet are in 100 yards?',
+//         answers: [
+//           { text: '300', correct: true },
+//           { text: '318.65', correct: false },
+//           { text: '303.33', correct: false },
+//           { text: '366.66', correct: false }
+//         ]
+//       },
+//       {
+//         question: 'How many miles are in 1 Nautical League?',
+//         answers: [
+//           { text: '6.215 miles', correct: false },
+//           { text: '3.45 miles', correct: true },
+//           { text: '12.12 miles', correct: false },
+//           { text: '9.02 miles', correct: false },
+//         ]
+//       },
+//       {
+//         question: 'What is the largest planet in the solar system?',
+//         answers: [
+//           { text: 'Jupiter', correct: true },
+//           { text: 'Saturn', correct: false },
+//           { text: 'Uranus', correct: false },
+//           { text: 'Pluto', correct: false }
+//         ]
+//       }
+//     ]
 
 
 $("#startQuiz").on("click", function() {
@@ -46,10 +62,6 @@ $("#startQuiz").on("click", function() {
             newSession.userName = $(".nameInput").val().trim();
             newSession.category = $(".categorySelected").val();
             startQuiz(newSession.category);
-            
-            
-
-
             
       })
 
