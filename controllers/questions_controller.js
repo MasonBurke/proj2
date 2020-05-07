8b3698730ef88eb6874dacee2a0c7bf071d11721
Mason Burke <masonburke@rocketmail.com>
2020-05-07 16:17:38 -0600

    final commit

--- controllers/questions_controller.js
+++ controllers/questions_controller.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
     //insert some stuff in here for the landing page
     res.render("index")
 })
@@ -9,19 +9,19 @@ router.get("/", function(req, res) {
 router.post("/api/questions", function (req, res) {
     //insert some stuff in here for post's if we end up using them :)
     //reference things in here and post to the page, ie like updates to the questions table ect
-    
+
 })
 
-router.put("/api/questions/:id", function(req, res) {
+router.put("/api/questions/:id", function (req, res) {
     //insert some stuff in here for the put's. ideally like targeting the q id and putting it on the page
     //this would be for updates of the score in the DB ect <3
 })
 
-router.delete("/api/questions/:id", function(req, res) {
+router.delete("/api/questions/:id", function (req, res) {
     //ya know... put the delete stuff here if we need it
     //not quick maths tbh
 })
-router.get("/questions", function (req,res) {
+router.get("/questions", function (req, res) {
     res.render("questions");
 })
 
@@ -29,7 +29,7 @@ router.get("/questions", function (req,res) {
 
 
 // mason was messing with these
-router.get("/highscores", function (req, res){
+router.get("/highscores", function (req, res) {
     res.render("highscore")
 })
 
