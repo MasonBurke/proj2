2d5db95a7225518f1c40565c65284697f091c414
Mason Burke <masonburke@rocketmail.com>
2020-05-08 08:52:32 -0600

    html routes updated

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,7 +2,6 @@ var path = require("path");
 
 module.exports = function(app) {
     app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/index2.html"));
         res.render("index");
     });
 
