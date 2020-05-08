bc61c0990912bdd831cb64d1ef88dc15b9e78a7b
Mason Burke <masonburke@rocketmail.com>
2020-05-08 11:30:41 -0600

    commit

--- server.js
+++ server.js
@@ -25,7 +25,12 @@ app.use(routes);
 
 require("./routes/score-api-routes")(app);
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+
+db.sequelize.sync().then(() => {
+  app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+
+})
+
